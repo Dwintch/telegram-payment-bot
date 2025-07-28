d8338a63a1a8568375799784ed3238f829e9de8e
Dwintch <Dwintch@yandex.ru>
2025-07-28 03:05:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+import threading
+from bot import run_bot1
+from bot2 import run_bot2
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=run_bot1)
+    t2 = threading.Thread(target=run_bot2)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
